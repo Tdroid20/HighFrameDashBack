89ca4344f375ac5586f7e01ee51b6aefbd7533eb
Tdroid2.0 <garotorocha90@gmail.com>
2022-06-24 17:23:10 -0300

    👋┃Start Project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Em breve
+# Atenção
+esse é um projeto feito com TypeScript
+
+# crie um arquivo `.env` na raís do projeto
+Exempo:
+```env 
+PORT=porta
+```
+
+Recomendado:
+```env
+PORT=3000
+```
--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const http = require('http');
 const express = require('express');
-const app = express()
+const c = require('colors')
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.json({ 
+        status: 'online',
+        version: '0.0.1',
+        isAuthenticate: 'unavailable'
+    })
+});
+
+const listener =  server.listen(process.env.PORT || 3000, function() {
+    console.log(
+        c.green(`Seu app está operando na porta `, 
+        c.yellow(listener.address().port)
+        )
+    );
+});
